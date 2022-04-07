0ab15a2bb9acdd70f302f7f03c8520c05dd5cf85
xe0xe <94848448+xe0xe@users.noreply.github.com>
2022-04-07 20:12:01 +0300

    Update users.py

--- data/users.py
+++ data/users.py
@@ -2,8 +2,10 @@ import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase. UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
@@ -19,3 +21,9 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
+    
+        def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
