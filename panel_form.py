9b4a38f49f4f8debdfc55161a0bfe94c348b7932
xe0xe <94848448+xe0xe@users.noreply.github.com>
2022-04-24 23:21:51 +0300

    Update panel_form.py

--- panel_form.py
+++ panel_form.py
@@ -5,5 +5,4 @@ from wtforms import SubmitField
 class PanelForm(FlaskForm):
     back = SubmitField('Назад')
     forward = SubmitField('Вперед')
-    month = SubmitField('За месяц')
-    year = SubmitField('За год')
+    add = SubmitField('Добавить')
