0ead9afb3430de8beb501389ef6e3a2f6a323365
xe0xe <94848448+xe0xe@users.noreply.github.com>
2022-04-25 11:45:08 +0300

    Update charge_form.py

--- charge_form.py
+++ charge_form.py
@@ -4,5 +4,4 @@ from wtforms import FloatField, IntegerField, SubmitField
 
 class ChargeForm(FlaskForm):
     content = FloatField("Содержание")
-    created_date = IntegerField('Номер месяца')
     submit = SubmitField('Применить')
